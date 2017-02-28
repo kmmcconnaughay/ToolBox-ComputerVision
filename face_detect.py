5c770a51252de49f2bd807237cae8dcb060e7472
Kerry McConnaughay <kmmcconnaughay@gmail.com>
2017-02-27 23:51:55 -0500

    Original attempt at using opencv to grab video frames and display them

--- face_detect.py
+++ face_detect.py
@@ -1,4 +1,25 @@
-""" Experiment with face detection and image filtering using OpenCV """
-
-import cv2
+"""This will grab video frames from the webcam and display them on screen"""
 import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0) #object that uses index to tell it how many cameras are
+# being used
+
+while True:
+    # Capture frame by frame
+    read_correctly, frame = cap.read(0)
+    assert read_correctly, "cap.read() failed"
+    print(cap.isOpened())
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # waitKey displays the image for the specified number of ms
+    if cv2.waitKey(10) & 0xFF == ord('q'): # clicking 'q' key will quit program
+        break
+
+# Release the capture when everything is done
+"""cap.release() returns a boolean. If the frame has been read correctly, it
+will be True."""
+cap.release()
+cv2.destroyAllWindows()
